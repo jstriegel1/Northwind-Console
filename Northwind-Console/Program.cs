c483fa724eebac14ce412ee54742fb3d86be3f38
Jennifer <jstriegel1@my.wctc.edu>
2019-11-06 09:53:59 -0600

    Option 2 - Create new Category

--- Northwind-Console/Program.cs
+++ Northwind-Console/Program.cs
@@ -5,48 +5,52 @@ using NorthwindConsole.Models;
 
 namespace NorthwindConsole
 {
-
-
-    namespace NorthwindConsole
+    class MainClass
     {
-        class MainClass
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public static void Main(string[] args)
         {
-            private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-            public static void Main(string[] args)
+            logger.Info("Program started");
+            try
             {
-                logger.Info("Program started");
-                try
+                string choice;
+                do
                 {
-                    string choice;
-                    do
+                    Console.WriteLine("1) Display Categories");
+                    Console.WriteLine("2) Add Category");
+                    Console.WriteLine("\"q\" to quit");
+                    choice = Console.ReadLine();
+                    Console.Clear();
+                    logger.Info($"Option {choice} selected");
+                    if (choice == "1")
                     {
-                        Console.WriteLine("1) Display Categories");
-                        Console.WriteLine("2) Add Category");
-                        Console.WriteLine("\"q\" to quit");
-                        choice = Console.ReadLine();
-                        Console.Clear();
-                        logger.Info($"Option {choice} selected");
-                        if (choice == "1")
+                        var db = new NorthwindContext();
+                        var query = db.Categories.OrderBy(p => p.CategoryName);
+
+                        Console.WriteLine($"{query.Count()} records returned");
+                        foreach (var item in query)
                         {
-                            var db = new NorthwindContext();
-                            var query = db.Categories.OrderBy(p => p.CategoryName);
-
-                            Console.WriteLine($"{query.Count()} records returned");
-                            foreach (var item in query)
-                            {
-                                Console.WriteLine($"{item.CategoryName} - {item.Description}");
-                            }
+                            Console.WriteLine($"{item.CategoryName} - {item.Description}");
                         }
-                        Console.WriteLine();
+                    }
+                    else if (choice == "2")
+                    {
+                        Category category = new Category();
+                        Console.WriteLine("Enter Category Name:");
+                        category.CategoryName = Console.ReadLine();
+                        Console.WriteLine("Enter the Category Description:");
+                        category.Description = Console.ReadLine();
+                        // TODO: save category to db
+                    }
+                    Console.WriteLine();
 
-                    } while (choice.ToLower() != "q");
-                }
-                catch (Exception ex)
-                {
-                    logger.Error(ex.Message);
-                }
-                logger.Info("Program ended");
+                } while (choice.ToLower() != "q");
             }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+            logger.Info("Program ended");
         }
     }
 }
