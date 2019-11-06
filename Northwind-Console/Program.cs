a0170cf30e254a8bbf1a9a5ddaa50398de264a4a
Jennifer <jstriegel1@my.wctc.edu>
2019-11-06 10:19:05 -0600

    Custom validation
    Check for unique CategoryName

--- Northwind-Console/Program.cs
+++ Northwind-Console/Program.cs
@@ -50,7 +50,20 @@ namespace NorthwindConsole
                         if (isValid)
                         {
                             logger.Info("Validation passed");
-                            // TODO: save category to db
+
+                            var db = new NorthwindContext();
+                            // check for unique name
+                            if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
+                            {
+                                // generate validation error
+                                isValid = false;
+                                results.Add(new ValidationResult("Name exists", new string[] { "CategoryName" }));
+                            }
+                            else
+                            {
+                                logger.Info("Validation passed");
+                                // TODO: save category to db
+                            }
                         }
                         if (!isValid)
                         {
